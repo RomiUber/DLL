ce234c365332f44012b6d01b54fa6f4074f6456b
erikayimga <108349476+erikayimga@users.noreply.github.com>
2022-11-19 18:08:31 +0100

    Update Personnes.sql

--- Tables/Personnes.sql
+++ Tables/Personnes.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Personnes (
+CREATE TABLE Personnes(
     Id_Personne VARCHAR(255) NOT NULL,
     Nom_Personne VARCHAR(255) NOT NULL,
     Telephone_Personne VARCHAR(255) NOT NULL
-) 
+)

8ecea00c04259c5e2ccb26d38505d77cf662549d
Stephanie Takam <stephanietakam1@gmail.com>
2022-11-19 22:48:46 +0000

    AJOUT CONTRAINTES

--- Tables/BesoinSelectionnable_contraintes.sql
+++ Tables/BesoinSelectionnable_contraintes.sql
@@ -1,2 +1,3 @@
 ALTER TABLE Besoin_Selectionnable ADD PRIMARY KEY (Id_besoin_select);
+
 ALTER TABLE Besoin_Selectionnable ADD CONSTRAINT fk_besoin_Select FOREIGN KEY (Id_Pressing) REFERENCES Pressing (Id_Pressing) ON DELETE CASCADE;
